94be532255d7c2fd6dedc5b9cb285e866c27b912
Salman Nur <salman.installer@gmail.com>
2022-12-16 20:36:11 +0600

    aa

--- src/C-Sharp-Feature/ABC ConsoleApp1/Program.cs
+++ src/C-Sharp-Feature/ABC ConsoleApp1/Program.cs
@@ -1,17 +1,40 @@
-﻿var arr = new[]                                         // Array of Anonymous Types
-{
-    new { Firstname = "Muhammad", Lastname = "Salman"},
-    new { Firstname = "faisal", Lastname = "mahmud"},
-    new { Firstname = "salman" , Lastname = "nur"}
+﻿
+using System;
 
-};
-
-foreach (var item in arr)
+class Program
 {
-    Console.WriteLine(item.Firstname + " " + item.Lastname);
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("allah");
+    }
+
 }
 
-for (int i = 0; i < arr.Length; i++)
-{
-    Console.WriteLine(arr[i].Firstname + " " + arr[i].Lastname);
-}
+
+
+
+
+
+
+
+
+
+
+//var arr = new[]                                         // Array of Anonymous Types
+//{
+//    new { Firstname = "Muhammad", Lastname = "Salman"},
+//    new { Firstname = "faisal", Lastname = "mahmud"},
+//    new { Firstname = "salman" , Lastname = "nur"}
+
+//};
+
+//foreach (var item in arr)
+//{
+//    Console.WriteLine(item.Firstname + " " + item.Lastname);
+//}
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    Console.WriteLine(arr[i].Firstname + " " + arr[i].Lastname);
+//}
---
+++ src/C-Sharp-Feature/Property-Auto property/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using Property_Auto_property;
+
+Teacher teacher = new Teacher();
+
+teacher.GenerateNewId();
+
+teacher.Id = "j" + "dfjdf";
+Console.WriteLine(teacher.Id);
